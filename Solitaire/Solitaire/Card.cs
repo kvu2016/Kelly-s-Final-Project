0b700ff82e07bb6d8e477744840c076fd1f74951
Kelly Vu <kvu2016@ou.edu>
2020-04-26 08:32:56 -0500

    f

---
+++ Solitaire/Solitaire/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire
+{
+    class Card
+    {
+        public Suit suit { get; set; }
+        public Value value { get; set; }
+        public enum Suit
+        {
+            clubs,
+            spades,
+            hearts,
+            diamionds
+        }
+        public enum Value
+        {
+            TWO = 2, three, four, five, six, seven, eight, nine, ten, jace, queen,
+             king, ace
+        }
+    
+    }
+}
