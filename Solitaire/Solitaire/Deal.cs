7b28d5380a5e6dc532d3e86c172378b0906d5cee
Kelly Vu <kvu2016@ou.edu>
2020-04-26 09:05:16 -0500

    k

---
+++ Solitaire/Solitaire/Deal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire
+{
+    private Card[] playerHand;
+    private Card[] computerHand;
+    class Deal: Deck
+    {
+        public DealCards()
+        {
+            playerHand = new Card[5];
+            sortedPlayerHand = new Card[5];
+            computerHand = new Card[5];
+            sortedComputerHand = new Card[5];
+        }
+    }
+}
---
+++ Solitaire/Solitaire/Deck.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire
+{
+    class Deck:Card
+    {
+        const int numberofcards = 52;//because there are 52 cards in a deck
+        private Card[] deck;//array of cards
+
+
+        public Deck()
+        {
+            deck = new Card[numberofcards];
+        }
+
+        public Card[] getcurrentdeck { get { return deck; } }
+        //gets the current deck of cards
+
+        public void setUpDeck()
+        {
+            int i = 0;
+            foreach (Suit s in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Value v in Enum.GetValues(typeof(Value)))
+                {
+                    deck[i] = new Card { suit = s, value = v };
+                    i++;
+                }
+            }
+
+            ShuffleCards();//using shuffle method
+        }
+
+        //shuffle the deck
+        public void ShuffleCards()
+        {
+            Random rand = new Random();
+            Card temporary;//temporary card given
+
+           
+            for (int shuffleamount = 0; shuffleamount < 2000; shuffleamount++)//shuffle 2000 times
+            {
+                for (int i = 0; i < numberofcards; i++)
+                {
+                    //give out random cards
+                    int card2 = rand.Next(13);
+                    temporary = deck[i];
+                    deck[i] = deck[card2];
+                    deck[card2] = temporary;
+                }
+            }
+        }
+    }
+}
+
